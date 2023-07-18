7a645f7b81c9e96850fa689e69121bed822d9195
Anuj Sharma <anujsharma@monocept.com>
2023-07-18 12:06:30 +0530

    add login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import Login from './src/components/Login'
 
-const App = () => {
+const Login = () => {
   return (
     <View>
-      <Login/>
+      <Text>Login</Text>
     </View>
   )
 }
-
 const styles = StyleSheet.create({})
-export default App
+export default Login
 
