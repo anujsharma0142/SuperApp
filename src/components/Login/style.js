b21d4f7acf866938ae78586157e7db7878243e9c
Anuj Sharma <anujsharma@monocept.com>
2023-07-21 13:10:11 +0530

    login page

---
+++ src/components/Login/style.js
@@ -0,0 +1,163 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 10,
+  },
+
+  imageStyle: {
+    width: 50,
+    height: 50,
+  },
+
+  headerContainer: {
+    marginTop: 30,
+    marginLeft: 20,
+  },
+
+  headerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#111',
+  },
+
+  textInputs: {
+    color: '#111',
+    fontSize: 15,
+    padding: 15,
+    borderWidth: 1,
+    borderColor: '#D8D8D8',
+    borderRadius: 10,
+    marginRight: 10,
+  },
+
+  button: {
+    backgroundColor: '#F28C28',
+    padding: 10,
+    borderRadius: 23,
+    alignItems: 'center',
+    marginTop: 10,
+    marginRight: 10,
+  },
+
+  buttonOutFill: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#D8D8D8',
+    borderRadius: 23,
+    padding: 12,
+    alignItems: 'center',
+    marginTop: 10,
+    marginRight: 10,
+  },
+
+  clickBtn: {
+    fontSize: 14,
+    marginLeft: 10,
+    color: '#111',
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+  },
+
+  trackBtn: {
+    paddingHorizontal: 4,
+    paddingVertical: 4,
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#111',
+  },
+
+  visitBtn: {
+    color: '#111',
+    marginTop: 14,
+    fontSize: 17,
+    fontWeight: '500',
+    textDecorationLine: 'underline',
+  },
+
+  quickLink: {
+    fontSize: 13,
+    fontWeight: 'bold',
+    color: 'gray',
+  },
+
+  textUnderline: {
+    marginTop: 25,
+    height: 1,
+    borderColor: '#d3d3d3',
+    borderWidth: 0.7,
+  },
+
+  outFillButton: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#D8D8D8',
+    padding: 12,
+    borderRadius: 23,
+    alignItems: 'center',
+    marginTop: 10,
+    marginRight: 10,
+  },
+
+  outFillBtn: {
+    fontSize: 16,
+    color: 'gray',
+    fontWeight: '500',
+  },
+
+  textInput: {
+    color: '#111',
+    fontSize: 15,
+    padding: 15,
+    borderWidth: 1,
+    borderColor: '#D8D8D8',
+    borderRadius: 10,
+    marginRight: 10,
+  },
+
+  outerCheckbox: {
+    marginTop: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  buttonInnerText: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 10,
+  },
+
+  whatsAppIcon: {
+    width: 15,
+    height: 15,
+    marginLeft: 2,
+  },
+
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+
+  vectorIcon: {
+    marginRight: 10,
+  },
+
+  container: {
+    marginTop: 10,
+    marginRight: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  border: {
+    flex: 1,
+    height: 0.7,
+    backgroundColor: '#DCDCDC',
+  },
+  text: {
+    paddingHorizontal: 10,
+  },
+});
+
+export default styles;
